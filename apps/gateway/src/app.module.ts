9647693e81c5378da8acf0e1841956a890e1e7c2
remziMehdizade <remzi.mehdizade@outlook.com>
2023-04-17 15:47:12 +0400

    AddFileService

--- apps/gateway/src/app.module.ts
+++ apps/gateway/src/app.module.ts
@@ -1,9 +1,32 @@
+import { ApolloGatewayDriver, ApolloGatewayDriverConfig } from '@nestjs/apollo';
 import { Module } from '@nestjs/common';
+import { GraphQLModule } from '@nestjs/graphql';
+import { IntrospectAndCompose, RemoteGraphQLDataSource } from '@apollo/gateway';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
+  imports: [
+    GraphQLModule.forRoot<ApolloGatewayDriverConfig>({
+      driver: ApolloGatewayDriver,
+
+      gateway: {
+        supergraphSdl: new IntrospectAndCompose({
+          subgraphs: [
+            {
+              name: 'users',
+              url: 'http://localhost:3000/graphql',
+            },
+            {
+              name: 'file-service',
+              url: 'http://localhost:3001/graphql',
+            },
+          ],
+        }),
+
+      },
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
